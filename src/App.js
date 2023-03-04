38cb311a3aeb25b668a9e5576ea35198d79a17e1
Vladislav Kalinovskij <119266924+vk300115@users.noreply.github.com>
2023-03-04 00:53:47 +0000

    initial layout and dependancies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import Button from '@mui/material/Button';
+
+import { Counter } from './components/Counter';
+import { Banner } from './components/Banner';
+
 export const App = () => {
-  return <div>Hello </div>;
+  return (
+    <div>
+      <Banner />
+      <Counter />
+    </div>
+  );
 }; 
